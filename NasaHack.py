a8a3a2170ed73729b34cc8d1b7bdb815b61122a5
D4RK PH1N1X <71398334+D4RK-PH1N1X@users.noreply.github.com>
2020-09-17 15:21:58 +0600

    Add files via upload

---
+++ NasaHack.py
@@ -0,0 +1,13 @@
+#HACK NASA#
+print("Hacking Nasa 20%")
+import os
+os.system("sleep 5")
+print("Hacking Nasa 50%")
+os.system("sleep 5")
+print("Hacking Nasa 80%")
+os.system("sleep 5")
+print("Hacking Nasa 100%")
+os.system("sleep 5")
+print("Successfully Hacked Nasa!")
+print("Go to your file manager & you will find a file where all info & pass are stored")
+print("Thanks. By Dark Phinix")
